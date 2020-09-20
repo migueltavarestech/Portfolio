37fc53ec10a6a9e07b8ac0d7001815424973e714
Miguel <mike.lo.123@hotmail.com>
2020-09-20 18:02:01 +0100

    fix CV left column without JS

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-function resizeDivs() {
-    var main = document.getElementById('detailedCV').offsetHeight;
-    var sidebar = document.getElementById('info').offsetHeight;
-    if (sidebar > main) {
-        main = sidebar;
-        document.getElementById('detailedCV').style.height = document.getElementById('info').style.height = main + 'px'
-    } else {
-        sidebar = main;
-        document.getElementById('info').style.height = document.getElementById('detailedCV').style.height = sidebar + 'px'
-    }
-}
-window.onload = resizeDivs;
+// function resizeDivs() {
+//     var main = document.getElementById('detailedCV').offsetHeight;
+//     var sidebar = document.getElementById('info').offsetHeight;
+//     if (sidebar > main) {
+//         main = sidebar;
+//         document.getElementById('detailedCV').style.height = document.getElementById('info').style.height = main + 'px'
+//     } else {
+//         sidebar = main;
+//         document.getElementById('info').style.height = document.getElementById('detailedCV').style.height = sidebar + 'px'
+//     }
+// }
+// window.onload = resizeDivs;
